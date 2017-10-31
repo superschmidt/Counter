0bc70e3426c8ac157608d543aa43e0852848e3f9
Gareth Warry <garethwarry@gmail.com>
2017-10-30 20:13:17 -0700

    Final Countdown
    First countdown timer to say Hello!

---
+++ Counter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine("Hello World in 10");
+            while (i > 0)
+            {
+                Console.WriteLine(i);
+                i = (i - 1);
+                Thread.Sleep(1000);
+            }
+            Console.Write("Hello World");
+            Console.ReadKey(true);          
+        }
+    }
+}
